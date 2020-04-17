696f645dcd6ae5cb109cd92b1c544a0357f091db
Akash Khan <akash4gb@gmail.com>
2020-04-17 19:42:29 +0600

    Custom linq Filter

---
+++ LinqPractice/queries/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public float Rating { get; set; }
+
+    }
+}
---
+++ LinqPractice/queries/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List < Movie >{
+                new Movie { Title = "A Beautiful Mind", Rating = 8.9f, Year = 2002 },
+                new Movie { Title = "Haksow Ridge", Rating = 8.0f, Year = 2010 },
+                new Movie { Title = "Planet Apps", Rating = 7.9f, Year = 2008 },
+                new Movie { Title = "Jurassic Park", Rating = 8.5f, Year = 2018 },
+            };
+
+            var query = movies.Filter(m => m.Year >= 2010);
+            foreach (var movie in query)
+            {
+                Console.WriteLine(movie.Title);
+            }
+
+        }
+    }
+}

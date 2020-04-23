7cdb2abfab4b99d5d7d8fd1b326cc67006f9dc00
Akash Khan <akash4gb@gmail.com>
2020-04-23 10:37:56 +0600

    Joining

---
+++ LinqPractice/FileProccessing/Manufacturer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileProccessing
+{
+    class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Headquarters { get; set; }
+        public int Year { get; set; }
+
+       
+
+        internal static Manufacturer ParseFromCsv(string line)
+        {
+            var values = line.Split(',');
+            return new Manufacturer
+            {
+                Name = values[0],
+                Headquarters = values[1],
+                Year = Int32.Parse(values[2])
+
+            };
+        }
+    }
+
+}
--- LinqPractice/FileProccessing/Program.cs
+++ LinqPractice/FileProccessing/Program.cs
@@ -10,14 +10,72 @@ namespace FileProccessing
         static void Main(string[] args)
         {
             var cars = ProccessFile("Fuel.csv");
+            var manufacturers = ProccessManufacturer("manufacturers.csv");
 
-            var query = cars.OrderByDescending(c => c.Combined).ThenBy(c=>c.Name) ;
+            //var query = cars.OrderByDescending(c => c.Combined).ThenBy(c=>c.Name) ;
 
-            foreach (var car in query.Take(10))
+
+            //join in query syntax
+            var query1 = from car in cars
+                         join manufacturer in manufacturers on car.Manufacturer equals manufacturer.Name
+                         orderby car.Combined descending, car.Name ascending
+                         select new
+                         {
+                             manufacturer.Headquarters,
+                             car.Name,
+                             car.Combined
+                         };
+            
+            //join with multiple key
+            var query1v2 = from car in cars
+                         join manufacturer in manufacturers
+                         on new { car.Manufacturer, car.Year } 
+                         equals new { Manufacturer = manufacturer.Name, manufacturer.Year}
+                         orderby car.Combined descending, car.Name ascending
+                         select new
+                         {
+                             manufacturer.Headquarters,
+                             car.Name,
+                             car.Combined
+                         };
+
+            //join in extension method syntax
+            var query2 =
+                cars.Join(manufacturers, c => c.Manufacturer, m => m.Name, (c, m) => new
+                {
+                    //c.Name,
+                    //c.Combined,
+                    //m.Headquarters,
+                    Car = c,
+                    Manufacturer = m
+
+                }).OrderByDescending(c => c.Car.Combined).ThenBy(c => c.Car.Name).Select(
+                    c => new
+                    {
+                        c.Car.Name,
+                        c.Car.Combined,
+                        c.Manufacturer.Headquarters
+
+                    }
+                    );
+
+
+
+            foreach (var car in query2.Take(10))
             {
-                Console.WriteLine($"{car.Name} : {car.Combined}");
+                Console.WriteLine($"{car.Headquarters} {car.Name} : {car.Combined}");
             }
-        } 
+        }
+
+        private static List<Manufacturer> ProccessManufacturer(string path)
+        {
+            var query = File.ReadAllLines(path)
+                 .Skip(1)
+                 .Where(l => l.Length > 0)
+                 .Select(selector: Manufacturer.ParseFromCsv)
+                 ;
+            return query.ToList();
+        }
 
         private static List<Car> ProccessFile(string path)
         {
